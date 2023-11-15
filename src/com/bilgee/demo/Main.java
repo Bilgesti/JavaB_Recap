845af1c878398f89b0f4a4a2edf46e98fa227e1c
Bilgesti <144493145+Bilgesti@users.noreply.github.com>
2023-11-15 14:53:03 +0100

    1

--- src/com/bilgee/demo/Main.java
+++ src/com/bilgee/demo/Main.java
@@ -3,23 +3,6 @@ package com.bilgee.demo;
 public class Main {
     public static void main(String[] args) {
 
-        // Instantiate
-        Counter counterA = new Counter();
-        Counter counterB = new Counter();
-        Counter counterC = new Counter();
-
-        counterA.increaseCount(); // 0 -> 1
-        counterB.increaseCount(); // 1 -> 2
-        counterC.increaseCount(); // 2 -> 3
-
-        Counter.sayHello();
-
-        // TODO Counter DOES NOT have static currently
-        // what is going to happen when we run the code now?
-
-        // TODO Why does each counter start with 0 ?
-        // Memory positions, they are NOT shared
-
 
     }
 }
